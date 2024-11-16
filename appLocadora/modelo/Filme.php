b0b76e3c59cb87c1ab1a217e34a967ad3e584224
Evillyn <130591438+htttpsviiiih@users.noreply.github.com>
2024-11-16 16:07:51 -0300

    Trabalho, primeiro comit
    tenta clonar direto pro teu vscode pra ver se ele roda

---
+++ appLocadora/modelo/Filme.php
@@ -0,0 +1,99 @@
+<?php
+
+require_once('Produto.php');
+require_once('IProduto.php');
+
+class Filme extends Produto implements IProduto{
+    private int $tempoDuracao;
+    private bool $continuidade;
+    private bool $cinema;
+    private bool $uniFisica;
+
+    public function getDadosProduto(){
+        $dados = "Filme: ".$this ->getNome()."\n";
+        $dados .= "Data de lançamento: ".$this->getDataLanc()."\n";
+        $dados .= "Classificação indicativa: ". $this->getClassIdicativa()."\n";
+        $dados .= "Gênero: ".$this-> getGenero()."\n";
+        $dados .= "Adaptação de livro: ".($this -> adptLivro ? "Sim" : "Não")."\n";
+        $dados .= "Tempo de duração: ".$this->getTempoDuracao()."\n";
+        $dados .= "Continuidade: ". ($this -> continuidade ? "Sim" : "Não")."\n";
+        $dados .= "Passou no cinema: ". ($this -> cinema ? "Sim" : "Não")."\n";
+        $dados .= "Está disponível em meio físico: ". ($this -> uniFisica ? "Sim" : "Não")."\n";
+        $dados .= "Está disponível em meio web: ". ($this -> dispoWeb ? "Sim" : "Não")."\n";
+        $dados .= "Preço: ". $this -> getPreco()."\n";
+        $dados .= "Diretor: \n". $this ->getDiretor()."\n";
+        return $dados;
+    }
+
+    /**
+     * Get the value of tempoDuracao
+     */
+    public function getTempoDuracao(): int
+    {
+        return $this->tempoDuracao;
+    }
+
+    /**
+     * Set the value of tempoDuracao
+     */
+    public function setTempoDuracao(int $tempoDuracao): self
+    {
+        $this->tempoDuracao = $tempoDuracao;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of continuidade
+     */
+    public function isContinuidade(): bool
+    {
+        return $this->continuidade;
+    }
+
+    /**
+     * Set the value of continuidade
+     */
+    public function setContinuidade(bool $continuidade): self
+    {
+        $this->continuidade = $continuidade;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of cinema
+     */
+    public function isCinema(): bool
+    {
+        return $this->cinema;
+    }
+
+    /**
+     * Set the value of cinema
+     */
+    public function setCinema(bool $cinema): self
+    {
+        $this->cinema = $cinema;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of uniFisica
+     */
+    public function isUniFisica(): bool
+    {
+        return $this->uniFisica;
+    }
+
+    /**
+     * Set the value of uniFisica
+     */
+    public function setUniFisica(bool $uniFisica): self
+    {
+        $this->uniFisica = $uniFisica;
+
+        return $this;
+    }
+}
